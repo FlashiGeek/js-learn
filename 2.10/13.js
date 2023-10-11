921105909138e1ef0a772321d1613012177ebaa7
FlashiGeek <turovskayalana@gmail.com>
2023-10-11 22:00:11 +0300

    2.10

---
+++ 2.10/13.js
@@ -0,0 +1,7 @@
+let oficialname = prompt('Какое «официальное» название JavaScript?', '');
+
+if (oficialname == 'ECMAScript') {
+  alert('Верно!');
+} else {
+  alert('Не знаете?“ECMAScript”!')
+}
---
+++ 2.10/14.js
@@ -0,0 +1,8 @@
+let userInput = prompt('Введите число', 0);
+if (userInput > 0) {
+  alert(1);
+} else if (userInput < 0) {
+  alert(-1);
+} else {
+  alert(0);
+}
