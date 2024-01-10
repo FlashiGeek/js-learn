779cf6dbfbda8769bb49121f447081dc3b2e3ef2
FlashiGeek <turovskayalana@gmail.com>
2024-01-11 00:43:38 +0300

    5.7

---
+++ 5.7/1.js
@@ -0,0 +1,11 @@
+function unique(arr) {
+    let uniqueValues = [...new Set(arr)];
+    return uniqueValues;
+    }
+    
+    // тестовые данные
+    let values = ["Hare", "Krishna", "Hare", "Krishna",
+    "Krishna", "Krishna", "Hare", "Hare", ":-O"
+    ];
+    
+    console.log(unique(values)); // ["Hare", "Krishna", ":-O"]
