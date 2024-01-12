4c74828ebc4305b6d0f5f748c0d13153a9d9d199
FlashiGeek <turovskayalana@gmail.com>
2024-01-12 22:14:28 +0300

    5.7

--- 5.7/1.js
+++ 5.7/1.js
@@ -1,11 +1,19 @@
 function unique(arr) {
-    let uniqueValues = [...new Set(arr)];
-    return uniqueValues;
-    }
-    
-    // тестовые данные
-    let values = ["Hare", "Krishna", "Hare", "Krishna",
-    "Krishna", "Krishna", "Hare", "Hare", ":-O"
-    ];
-    
-    console.log(unique(values)); // ["Hare", "Krishna", ":-O"]
+  let uniqueValues = [...new Set(arr)];
+  return uniqueValues;
+}
+
+// тестовые данные
+let values = [
+  "Hare",
+  "Krishna",
+  "Hare",
+  "Krishna",
+  "Krishna",
+  "Krishna",
+  "Hare",
+  "Hare",
+  ":-O",
+];
+
+console.log(unique(values)); // ["Hare", "Krishna", ":-O"]
